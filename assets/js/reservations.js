4046d1742c429dff4b2ba060c4368e60b298646a
Krinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-01-18 18:24:23 +0200

    to colorization dulefki aftomata pleon

--- assets/js/reservations.js
+++ assets/js/reservations.js
@@ -1,43 +1,48 @@
 $(document).ready(function () {
 
-//TODO: run the colorize function stin arxi tu load
 
-$(".resvPopup").click(function () {
-    //otan fernw ta data me ajax or laravel prp na fernw j to id etsi wste kathe fora
-    //pu kamnei run tunto function na kamnw query to id j na allassw ta data tu modal
-    $("#issueModal").modal('show')
-})
+    $(".resvPopup").click(function () {
+        //otan fernw ta data me ajax or laravel prp na fernw j to id etsi wste kathe fora
+        //pu kamnei run tunto function na kamnw query to id j na allassw ta data tu modal
+        $("#issueModal").modal('show')
+    })
 
-//On click sto modify reservvation tu modal
-$("#modResv").click(function () {
-    window.location.href = "user2-editres.html";
-})
+    //On click sto modify reservvation tu modal
+    $("#modResv").click(function () {
+        window.location.href = "user2-editres.html";
+    })
 
-//If attendance changes in the modal
-$("#attendance").change(function () {
-    //TODO: update the table and the database
 
-    colorizeTable();
-})
 
-//TODO:
-//On every change 
-    //DATE changed
-    //attendance changed
-    //time has past
-$("#mydate").change(function () {
-    //TODO: get data if date changed
-
-    // run the colorize function
+    //run the colorize function stin arxi tu load
     colorizeTable()
-})
+
+    //Colorize runs when:
+        //DATE changed
+        //attendance changed
+        //trigger time has passed
+        //the document is loaded (stin arxi)
+
+    $("#mydate").change(function () {
+        //TODO: get data if date changed
+
+        // run the colorize function
+        colorizeTable()
+    })
+
+    //If attendance changes in the modal
+    $("#attendance").change(function () {
+        //TODO: update the table and the database
+
+        colorizeTable();
+    })
 
 });
 
 
 
 //This function is used to colorize the table
-function colorizeTable(){
+function colorizeTable() {
 
     //TODO: Remove any previous formatting
     $("tr").each(function () {
@@ -46,54 +51,56 @@ function colorizeTable(){
         $(this).removeClass("table-success")
     })
 
+    trigger_time = 2000; //because max time is 24*60 + 59 = 1499
+
+    //Get Current Time and change to minutes
+    now = new Date();
+    curr_time = now.getHours() * 60 + now.getMinutes()
+
     //Select all td elements with class=time
     $("tr > td.time").each(function () {
 
         //Change resv time to minutes
-        resv_time = parseInt($(this).text().split(':')[0],10) * 60 + parseInt($(this).text().split(':')[1],10)
-        
-        //Get Current Time and change to minutes
-        now = new Date();
-        curr_time = now.getHours() *60 + now.getMinutes()
+        resv_time = parseInt($(this).text().split(':')[0], 10) * 60 + parseInt($(this).text().split(':')[1], 10)
 
         //Get attendance
-        attendance = parseInt($(this).siblings("td").children("span.attendance").text(),10)
+        attendance = parseInt($(this).siblings("td").children("span.attendance").text(), 10)
 
         //Get people pu enartoun
-        people = parseInt($(this).siblings("td").children("span.people").text(),10)
+        people = parseInt($(this).siblings("td").children("span.people").text(), 10)
 
         //////Check if any reservations have a passed time and no attendance
-        if (curr_time > resv_time && !attendance){
+        if (curr_time >= resv_time && !attendance) {
             // this <tr>'s father na allaksi class se danger
             $(this).parent().addClass("table-danger")
-        
-        //else if attendance is < people
-        }else if(attendance>0 && attendance < people){
+
+            //else if attendance is < people
+        } else if (attendance > 0 && attendance < people) {
             // this <tr>'s father na allaksi class se info
             $(this).parent().addClass("table-info")
 
-        //else if attendance is equal to people
-        }else if(attendance == people){
+            //else if attendance is equal to people
+        } else if (attendance == people) {
             // this <tr>'s father na allaksi class se success
             $(this).parent().addClass("table-success")
-        }   
+        }
 
+
+        //check if its the smallest so far
+        if (!attendance && (curr_time < resv_time) && (resv_time < trigger_time)) {
+            trigger_time = resv_time;
+        }
     })
+
+    //If the trigger is the default, dont set a trigger
+    if (trigger_time != 2000) {
+        triggerSet(trigger_time-curr_time);
+    }
+
 }
 
-
-
-//TODO:
-// //Runs every 1 minute
-// //Evre to pio kontino available time me to date.now
-// //calculate posi ora ena perasi ospu na erti
-// //set the timer
-// setInterval(function() {
-//     // your code goes here...
-// }, 60 * 1000); // 60 * 1000 milsec
-
-// function fn60sec() {
-//     // runs every 60 sec and runs on init.
-// }
-// fn60sec();
-// setInterval(fn60sec, 60*1000);
+//Triggers colorize everytime the closest reservation is due
+//colorize function sets the trigger time again to the next closest
+function triggerSet(trigger_time) {
+    setInterval(colorizeTable, trigger_time * 60 * 1000); // 60 * 1000milisec = 1 minute so we calculate the minutes
+}
