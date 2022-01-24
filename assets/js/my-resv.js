e798bf191487cd4a131ffa0d73181148303a5b18
Krinos <krinos.vasiliou@eecei.cut.ac.cy>
2022-01-24 18:42:54 +0200

    rating works with notifications and buttons

--- assets/js/my-resv.js
+++ assets/js/my-resv.js
@@ -1,8 +1,55 @@
 $(document).ready(function () {
 
-$(".rate").click(function () {
+var raterow;
+
+//Update notifications
+notification_update()
+
+
+//When user clicks on Rate Now!
+$(".rate").click(function (e) {
+    //Show the rating modal
     $("#small").modal('show')
-    //alert("ok")
+
+    //Get the element clicked
+    raterow = $(this).parents("td")
+})
+
+
+//When user confirms rating
+$("#rateConfirm").click(function(){
+    //TODO: checkdb first if its okay
+
+    //Replace button with text "Rating added"
+    raterow.html("Rating added")
+
+    //Update the notification
+    notification_update()
+
+
+    //Get the row id that is selected (useful for db)
+    //raterow.parent().attr("id")
 })
 
 })
+
+
+
+function notification_update(){
+    var num;
+    //Find number of buttons shown
+    num = $("td > div > button").length
+
+    //Check if >0
+    if (num>0){
+        //Find the span and replace the number and turn to shown
+        $("#notification").html(num);
+        $("#notification").show()
+    }
+    else{
+        //Else turn to hidden
+        $("#notification").hide()
+    }
+    
+
+}
